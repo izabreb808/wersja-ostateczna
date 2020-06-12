5f26cf6558ac8e578c26be65c5afea95ece788cb
izabreb808 <56292825+izabreb808@users.noreply.github.com>
2020-06-12 18:37:27 +0200

    Create funkcje.h

---
+++ funkcje.h
@@ -0,0 +1,23 @@
+#ifndef FUNKCJE_H
+#define FUNKCJE_H
+#include <iostream>
+
+using namespace std;
+
+string wczytajposilek();
+
+double iloscpobierz(double il);
+
+void ilosczamien(double il);
+
+void podsumujkalorie(double il);
+
+void czynowydzien(double il);
+
+double wczytajkalorie(double il);
+
+void wpiszdopliku(string posilek, double kcal);
+
+void dziennik();
+
+#endif
